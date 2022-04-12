af1d2702b830cf0dbdbd483be6ff89ffe50fa1e2
Mihajlo Starcevic <mihajlomixy@gmail.com>
2022-04-12 15:02:40 +0200

    Added login UI.

--- frontend/src/components/pages/login/login.tsx
+++ frontend/src/components/pages/login/login.tsx
@@ -1,9 +1,50 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { AppRoutes } from "../../../consts/routes";
 
 export const Login = () => {
+  const navigate = useNavigate();
+
+  const navigateToRegister = () => {
+    navigate(AppRoutes.register);
+  };
+
   return (
     <React.Fragment>
-      <p>Login page</p>
+      <div className="uk-flex uk-flex-column uk-flex-center uk-flex-middle uk-margin-xlarge-top">
+        <div className="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+          <h3 className="uk-card-title">{"Welcome to $PROJ_NAME!"}</h3>
+          <form>
+            <fieldset className="uk-fieldset">
+              <label htmlFor="emailField">{"Email:"}</label>
+              <input
+                className="uk-input uk-margin-bottom"
+                id="emailField"
+                type="text"
+              ></input>
+
+              <label htmlFor="passwordField">{"Password:"}</label>
+              <input
+                className="uk-input"
+                id="passwordField"
+                type="password"
+              ></input>
+
+              <p className="uk-margin-bottom">
+                {"Don't have an account? "}
+                <a onClick={navigateToRegister}>{"Register new account."}</a>
+              </p>
+
+              <div className="uk-flex uk-flex-center">
+                <button className="uk-button uk-button-primary">
+                  {"Login"}
+                </button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
