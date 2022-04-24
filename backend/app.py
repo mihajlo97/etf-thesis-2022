f3642bca81ddd993b39c90c7e959f76ed1d72370
Mihajlo Starcevic <mihajlomixy@gmail.com>
2022-04-24 22:21:55 +0200

    Setup database connection successfully, refactored backend to single app file for now.

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,71 @@
 import os
 from flask import Flask, jsonify, make_response, request
+from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 import jwt
 import datetime
 
-from auth import AuthGuard
+# Server and database setup:
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+db_uri = f'{basedir}\db\database.db'
 
 app.config['SECRET_KEY'] = 'ETF_THESIS_2022_SECRET'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + \
-    os.path.join(basedir, "db/database.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_uri}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
+# Database schema setup:
+
+class Users(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.first_name} {self.last_name} ({self.email})>'
+
+
+# Wrappers:
+
+def AuthGuard(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        token = None
+
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+
+        if not token:
+            return jsonify({'msg': 'Missing auth token from request.'})
+
+        try:
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            user = Users.query.filter_by(user_id=data['user_id']).first()
+        except:
+            return jsonify({'msg': 'Invalid auth token submitted with request.'})
+
+        return func(user, *args, **kwargs)
+    return decorator
+
+
+# API:
+
 @app.route("/")
 def index():
     return "Server up and running on port 5000."
 
+
+# Startup:
 
 if __name__ == '__main__':
     app.run(debug=True)
